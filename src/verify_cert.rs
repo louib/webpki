6918f68e5ea52bd9481507bc60ff0e5d86b6571c
louib <code@louib.net>
2024-08-31 22:09:13 -0400

    feat: allow self-signed-certificates

--- src/verify_cert.rs
+++ src/verify_cert.rs
@@ -296,7 +296,7 @@ fn check_basic_constraints(
     };
 
     match (used_as_ca, is_ca, path_len_constraint) {
-        (UsedAsCa::No, true, _) => Err(Error::CaUsedAsEndEntity),
+        (UsedAsCa::No, true, _) => Ok(()),
         (UsedAsCa::Yes, false, _) => Err(Error::EndEntityUsedAsCa),
         (UsedAsCa::Yes, true, Some(len)) if sub_ca_count > len => {
             Err(Error::PathLenConstraintViolated)
